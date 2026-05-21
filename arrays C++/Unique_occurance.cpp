6187c5b73b0d99bf73eb967498ecc8918979844f
R Aditya Murty <adityamurty2006@gmail.com>
2026-05-21 19:29:17 +0530

    unique occurrence

---
+++ arrays C++/Unique_occurance.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[4] = {1,2,2,3};
+    int freq[10] = {0};
+
+    // frequency count
+    for(int i = 0; i < 4; i++){
+        freq[arr[i]]++;
+    }
+
+    // uniqueness check
+    int i=0;
+    while(i<10){
+        int j=i+1;
+        while(j<10){
+            if(freq[i]==freq[j]&& freq[j]>0){
+                cout<<"False"<< endl;
+                return 0;
+            }
+            j++;
+        }
+        i++;
+    }
+
+    for(int i=0; i<10; i++){
+        if(freq[i]>0){
+            cout<< i << " -> " << freq[i] << endl;
+        }
+    }
+
+    return 0;
+}
